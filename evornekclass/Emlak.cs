e40dce94a565ba953bd9c6dd883de2f24f388659
kevrrana <145452208+kevrrana@users.noreply.github.com>
2023-11-28 12:06:57 +0300

    Add files via upload

---
+++ evornekclass/Emlak.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evornekclass
+{
+    internal class Emlak
+    {
+        int fiyati;
+        string adres;
+        int odasayisi;
+        string asansor;
+        int binayasi;
+        int binakatsayisi;
+        string kullanimdurumu;
+        int bulundugukat;
+        string tapudurumu;
+        string binarengi;
+
+        public int Fiyati { get => fiyati; set => fiyati = value; }
+        public string Adres { get => adres; set => adres = value; }
+        public int Odasayisi { get => odasayisi; set => odasayisi = value; }
+        public string Asansor { get => asansor; set => asansor = value; }
+        public int Binayasi { get => binayasi; set => binayasi = value; }
+        public int Binakatsayisi { get => binakatsayisi; set => binakatsayisi = value; }
+        public string Kullanimdurumu { get => kullanimdurumu; set => kullanimdurumu = value; }
+        public int Bulundugukat { get => bulundugukat; set => bulundugukat = value; }
+        public string Tapudurumu { get => tapudurumu; set => tapudurumu = value; }
+        public string Binarengi { get => binarengi; set => binarengi = value; }
+
+        public Emlak(int fiyati, string adres, int odasayisi, string asansor, int binayasi, int binakatsayisi, string kullanimdurumu, int bulundugukat, string tapudurumu, string binarengi)
+        {
+            this.fiyati = fiyati;
+            this.adres = adres;
+            this.odasayisi = odasayisi;
+            this.asansor = asansor;
+            this.binayasi = binayasi;
+            this.binakatsayisi = binakatsayisi;
+            this.kullanimdurumu = kullanimdurumu;
+            this.bulundugukat = bulundugukat;
+            this.tapudurumu = tapudurumu;
+            this.binarengi = binarengi;
+        }
+    }
+}
+
---
+++ evornekclass/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace evornekclass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(-124, 43);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(-121, 122);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(-362, 17);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
